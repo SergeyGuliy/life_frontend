30f33ee9a6b3be9eaba6e048f3143c2a99af3719
sergey <SergeyGuliy>
2021-06-18 20:03:43 +0300

    Refactoring Sidebar

--- src/components/layouts/SideBar.vue
+++ src/components/layouts/SideBar.vue
@@ -1,5 +1,27 @@
 <template>
   <v-navigation-drawer absolute temporary :value="value" @input="updateDrawer">
+    <template v-slot:prepend>
+      <v-list-item two-line>
+        <v-list-item-avatar>
+          <UserAvatar :userData="$user" />
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>Jane Smith</v-list-item-title>
+          <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link @click="$router.push('Cabinet')">
+        <v-list-item-icon>
+          <v-icon>mdi-settings</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ $t("layout.cabinet") }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </template>
+    <v-divider></v-divider>
     <v-list>
       <v-list-item
         v-for="(item, index) in items"
@@ -31,6 +53,9 @@
 <script>
 export default {
   name: "SideBar",
+  components: {
+    UserAvatar: () => import("../UserAvatar")
+  },
   props: {
     value: {
       type: Boolean,
@@ -45,11 +70,11 @@ export default {
           icon: "mdi-view-dashboard",
           linkParams: { name: "Home" }
         },
-        {
-          title: "layout.cabinet",
-          icon: "mdi-account-settings",
-          linkParams: { name: "Cabinet" }
-        },
+        // {
+        //   title: "layout.cabinet",
+        //   icon: "mdi-account-settings",
+        //   linkParams: { name: "Cabinet" }
+        // },
         {
           title: "layout.friends",
           icon: "mdi-account-group",
