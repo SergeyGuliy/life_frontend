79bbba0b0a1696d490a575ec334fb95043ae5b55
sergey <sergey.guliy.92@gmail.com>
2022-08-28 21:19:48 +0300

    setup alias to linked repository

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,20 @@
+const path = require("path");
+
 module.exports = {
   transpileDependencies: ["vuetify"],
   devServer: {
     host: "0.0.0.0",
     port: 8080
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@constants": path.resolve(
+          __dirname,
+          "node_modules/life_shared/constants"
+        ),
+        "@enums": path.resolve(__dirname, "node_modules/life_shared/enums")
+      }
+    }
   }
 };
