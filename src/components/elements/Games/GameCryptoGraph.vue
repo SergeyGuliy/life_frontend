e72c7fbfbd7af132f99ee18f7839aab0bf7aaec4
sergey <sergey.guliy.92@gmail.com>
2022-10-31 03:01:29 +0200

    created logic for watching change crypto price, and setup chart js

--- src/components/elements/Games/GameCryptoGraph.vue
+++ src/components/elements/Games/GameCryptoGraph.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>{{ crypto }}</div>
+  <line-chart
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :dataset-id-key="datasetIdKey"
+    :width="width"
+    :height="height"
+  />
 </template>
 
 <script>
@@ -7,30 +13,90 @@
 
 import { api } from "@/utils/api";
 
+import { Line } from "vue-chartjs/legacy";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  CategoryScale
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  CategoryScale
+);
 export default {
   name: "GameCryptoGraph",
 
   data() {
     return {
-      loading: true
+      datasetIdKey: "label",
+      width: 400,
+      height: 400,
+
+      chartData: {
+        labels: [],
+        datasets: [
+          {
+            label: this.crypto.name,
+            backgroundColor: "#f87979",
+            data: []
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true
+      },
+
+      loading: true,
+
+      cryptoHistory: null
     };
   },
+
+  components: { "line-chart": Line },
 
   props: {
     crypto: {}
   },
 
-  created() {
-    console.log(this.$gameId);
+  $initSocketListener() {
     api.games.crypto
       .getCrypto({
         name: this.crypto.name,
         gameId: this.$gameId
       })
-      .then(data => {
-        console.log(data);
+      .then(cryptoHistory => {
+        cryptoHistory
+          .filter(({ date }) => date.monthCode && date.year)
+          .forEach(({ date, currentPrice }) => {
+            this.chartData.datasets[0].data.push(currentPrice);
+            this.chartData.labels.push(`${date.monthCode} ${date.year}`);
+          });
         this.loading = false;
+
+        this.$socketInit({
+          games_tick: this.tickGameData
+        });
       });
+  },
+  methods: {
+    tickGameData({ cryptos, date }) {
+      let { currentPrice } = cryptos.find(
+        crypto => crypto.name === this.crypto.name
+      );
+      this.chartData.datasets[0].data.push(currentPrice);
+      this.chartData.labels.push(`${date.monthCode} ${date.year}`);
+    }
   }
 };
 </script>
